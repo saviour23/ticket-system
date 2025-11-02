2cb1a9c644e9b79dc59e7b7d7eb1b256c8d470d3
Neeraj Negi <negi.neerajnegi@gmail.com>
2025-11-02 14:09:08 +1100

    Spring secutiry enabled

---
+++ src/main/java/com/au/sportsbet/ticket/system/api/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.au.sportsbet.ticket.system.api.config;
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
+
+                                // Allow access to the specific API URL
+                                .requestMatchers("/api/transactions").permitAll()
+
+                                .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
--- src/test/java/com/au/sportsbet/ticket/system/api/controller/TicketTransactionControllerTest.java
+++ src/test/java/com/au/sportsbet/ticket/system/api/controller/TicketTransactionControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(TicketTransactionController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class TicketTransactionControllerTest {
 
     @Autowired
